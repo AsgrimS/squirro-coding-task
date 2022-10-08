02829cc3c350f2f8b25e8f775fe5f0805e028fa6
AsgrimS <jan.semik@gmail.com>
2022-10-08 11:14:53 +0200

    caching

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
 
 from app.routers import documents
 
 app = FastAPI()
 
 app.include_router(documents.router, prefix="/documents", tags=["documents"])
+
+
+@app.on_event("startup")
+async def startup():
+    FastAPICache.init(backend=InMemoryBackend(), prefix="fastapi-cache")
 
 
 @app.get("/")
--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,7 +1,8 @@
-from fastapi import Depends, status
+from fastapi import Depends, Query, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException
 from fastapi.routing import APIRouter
+from fastapi_cache.decorator import cache
 
 from app.database import get_documents_collection
 from app.models.documents import Document, DocumentBase, DocumentInput
@@ -10,9 +11,7 @@ from app.summarize import get_summarization
 router = APIRouter()
 
 
-@router.post(
-    "/", response_description="Add new document", response_model=DocumentBase, status_code=status.HTTP_201_CREATED
-)
+@router.post("/", description="Add a new document.", response_model=DocumentBase, status_code=status.HTTP_201_CREATED)
 async def create_document(document: DocumentInput, documents_collection=Depends(get_documents_collection)):
     document = jsonable_encoder(document)
     new_document = await documents_collection.insert_one(document)
@@ -20,13 +19,13 @@ async def create_document(document: DocumentInput, documents_collection=Depends(
     return created_document
 
 
-@router.get("/", response_description="List all documents", response_model=list[DocumentBase])
+@router.get("/", description="List all documents titles and ids.", response_model=list[DocumentBase])
 async def list_documents(documents_collection=Depends(get_documents_collection)):
     documents = await documents_collection.find({}, {"title": 1}).to_list(None)
     return documents
 
 
-@router.get("/{doc_id}", response_description="Get a single document", response_model=Document)
+@router.get("/{doc_id}", description="Get a single document by id.", response_model=Document)
 async def get_document(doc_id: str, documents_collection=Depends(get_documents_collection)):
     if (document := await documents_collection.find_one({"_id": doc_id})) is not None:
         return document
@@ -34,7 +33,7 @@ async def get_document(doc_id: str, documents_collection=Depends(get_documents_c
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Document {doc_id} not found")
 
 
-@router.delete("/{doc_id}", response_description="Delete a document", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{doc_id}", description="Delete a single document by id.", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_document(doc_id: str, documents_collection=Depends(get_documents_collection)):
     results = await documents_collection.delete_one({"_id": doc_id})
     if results.deleted_count == 1:
@@ -43,9 +42,14 @@ async def delete_document(doc_id: str, documents_collection=Depends(get_document
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Document {doc_id} not found")
 
 
-@router.get("/summarize/{doc_id}", response_description="Summarize a document")
+@router.get("/summarize/{doc_id}", description="Summarize a single document by id. Cached for 10 minutes.")
+@cache(expire=60 * 10)
 async def summarize_document(
-    doc_id: str, summary_percentage: float, documents_collection=Depends(get_documents_collection)
+    doc_id: str,
+    summary_percentage: float = Query(
+        description="Size of the summary compared to the article in %.", gt=0, le=1, default=0.05
+    ),
+    documents_collection=Depends(get_documents_collection),
 ):
     if (document := await documents_collection.find_one({"_id": doc_id})) is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Document {doc_id} not found")
